cd1f41eef37ab9368bfed1f377dcfb1d1a39280f
creativekoen <creativekoen@gmail.com>
2015-06-05 22:44:23 +0200

    ask for stylus or sass

--- app/index.js
+++ app/index.js
@@ -13,6 +13,7 @@ var myflow = yeoman.generators.Base.extend({
         this.log(yosay(
             'Welcome to CreativeKoen\'s workflow generator!'
         ));
+        // this.log('\n This personal framework comes with jade,php,html,stylus,sass,lost,bower,browserSync etc..')
     },
 
     promtUser: function() {
@@ -28,7 +29,39 @@ var myflow = yeoman.generators.Base.extend({
             name: 'localName',
             message: 'what is your localhost vhosts name ?'
             ,default: 'local.best-project-ever'
-        }];
+        }
+        ,{
+            type: 'confirm',
+            name: 'php server',
+            message: 'Do you me to spin up a php server ?',
+            default: false
+        }
+        ,{
+            type: 'checkbox',
+            name: 'prepros',
+            message: 'Which css prepocessor do you want to use ?',
+            choices: [
+            {
+                value: 'preStylus',
+                name: 'Stylus',
+                checked: true
+            },
+            {
+                value: 'preSass',
+                name: 'Sass',
+                checked: false
+            }]
+        }
+        ,{
+            type: 'confirm',
+            name: 'gridSystem',
+            message: 'Do you want to use the Lost grid system ?',
+            default: true
+        }
+
+
+
+        ];
 
         this.prompt(prompts, function(answers){
 
@@ -37,109 +70,77 @@ var myflow = yeoman.generators.Base.extend({
 
             done();
         }.bind(this));
-    },//end promtUser
-
-
-    copyFiles: function(){
-
-        var approot = base+this.includeAppName+'/';
-
-        this.template('.gitignore',    approot+'.gitignore');
-        this.template('editorconfig',  approot+'editorconfig');
-        this.template('README.md',     approot+'README.md');
-        this.template('bower.json',     approot+'bower.json');
-
-        // passing app name to package.json
-        var appname = { clientname: this.includeAppName};
-        this.template('package.json', approot+'package.json', appname);
-
-        // passing localhost name to gulpfile.js
-        var localname = { localhost: this.includeLocalName};
-        this.template('gulpfile.js', approot+'gulpfile.js', localname);
-
-        var installDep = { newroot: approot};
-        this.template('_yo-rc.json', approot+'.yo-rc.json', installDep);
-    },//end copy files
-
-    move_ASSETS_Files: function(){
-        var approot = base+this.includeAppName+'/';
-        //index php
-        var clientname2 = { clientname: this.includeAppName};
-        this.template('source/index.php',approot+'source/index.php', clientname2);
-        // main js file
-        this.template('source/js/main.js',approot+'source/js/main.js');
-        // main scss file
-        this.template('source/scss/main.scss',approot+'source/scss/main.scss');
-
-        // smacks
-        this.template('source/scss/vars/_base.scss',approot+'source/scss/vars/_base.scss');
-        this.template('source/scss/vars/_fonts.scss',approot+'source/scss/vars/_fonts.scss');
-        this.template('source/scss/vars/_state.scss',approot+'source/scss/vars/_state.scss');
-        this.template('source/scss/vars/_theme.scss',approot+'source/scss/vars/_theme.scss');
-        this.template('source/scss/vars/_layout.scss',approot+'source/scss/vars/_layout.scss');
-        this.template('source/scss/vars/_navbar.scss',approot+'source/scss/vars/_navbar.scss');
-        this.template('source/scss/vars/_vars.scss',approot+'source/scss/vars/_vars.scss');
-
-        // griddle
-        this.template('source/scss/vars/_griddle-build.scss',approot+'source/scss/vars/_griddle-build.scss');
-        this.template('source/scss/vars/_griddle-lg.scss',approot+'source/scss/vars/_griddle-lg.scss');
-        this.template('source/scss/vars/_griddle-md.scss',approot+'source/scss/vars/_griddle-md.scss');
-        this.template('source/scss/vars/_griddle-sm.scss',approot+'source/scss/vars/_griddle-sm.scss');
-        this.template('source/scss/vars/_griddle-xs.scss',approot+'source/scss/vars/_griddle-xs.scss');
-        this.template('source/scss/vars/_griddle.scss',approot+'source/scss/vars/_griddle.scss');
-
-        // breakpoint
-        this.template('source/scss/vars/_breakpoint.scss',approot+'source/scss/vars/_breakpoint.scss');
-
-        this.template('source/scss/vars/breakpoint/_context.scss',approot+'source/scss/vars/breakpoint/_context.scss');
-        this.template('source/scss/vars/breakpoint/_helpers.scss',approot+'source/scss/vars/breakpoint/_helpers.scss');
-        this.template('source/scss/vars/breakpoint/_no-query.scss',approot+'source/scss/vars/breakpoint/_no-query.scss');
-        this.template('source/scss/vars/breakpoint/_respond-to.scss',approot+'source/scss/vars/breakpoint/_respond-to.scss');
-
-        this.template('source/scss/vars/breakpoint/_parsers.scss',approot+'source/scss/vars/breakpoint/_parsers.scss');
-        this.template('source/scss/vars/breakpoint/parsers/_double.scss',approot+'source/scss/vars/breakpoint/parsers/_double.scss');
-        this.template('source/scss/vars/breakpoint/parsers/_query.scss',approot+'source/scss/vars/breakpoint/parsers/_query.scss');
-        this.template('source/scss/vars/breakpoint/parsers/_resolution.scss',approot+'source/scss/vars/breakpoint/parsers/_resolution.scss');
-        this.template('source/scss/vars/breakpoint/parsers/_single.scss',approot+'source/scss/vars/breakpoint/parsers/_single.scss');
-        this.template('source/scss/vars/breakpoint/parsers/_triple.scss',approot+'source/scss/vars/breakpoint/parsers/_triple.scss');
-
-        this.template('source/scss/vars/breakpoint/parsers/double/_default-pair.scss',approot+'source/scss/vars/breakpoint/parsers/double/_default-pair.scss');
-        this.template('source/scss/vars/breakpoint/parsers/double/_default.scss',approot+'source/scss/vars/breakpoint/parsers/double/_default.scss');
-        this.template('source/scss/vars/breakpoint/parsers/double/_double-string.scss',approot+'source/scss/vars/breakpoint/parsers/double/_double-string.scss');
-
-        this.template('source/scss/vars/breakpoint/parsers/resolution/_resolution.scss',approot+'source/scss/vars/breakpoint/parsers/resolution/_resolution.scss');
-
-        this.template('source/scss/vars/breakpoint/parsers/single/_default.scss',approot+'source/scss/vars/breakpoint/parsers/single/_default.scss');
-        this.template('source/scss/vars/breakpoint/parsers/triple/_default.scss',approot+'source/scss/vars/breakpoint/parsers/triple/_default.scss');
-    }, //end move_ASSETS_Files
-    end: function () {
-
-        this.log(chalk.yellow(
-            '\n Go and get yourself a coffee, It will take a while...'
-        ));
-        //  new folder thats created with the appname
-        var approot = "\\"+base+this.includeAppName+"\\";
-        // add that to the current directory
-        var npmdir = process.cwd()+approot;
-
-        process.chdir(npmdir);
-
-        this.installDependencies({
-            callback: function(){
-                this.log(chalk.yellow(
-                    '\n Enjoying your coffee ? I\'m almost done...'
-                ));
-                this.spawnCommand('gulp',['build']);
-                this.spawnCommand('gulp',['vendor']);
-                // this.spawnCommand('gulp');
-            }.bind(this) // bind the callback to the parent scope
-        });
-    }
-    ,whatToDoNext: function() {
-        this.on('end', function(){
-            this.log(chalk.yellow("\n and read the README file"));
-            this.log(chalk.yellow("\n run")+chalk.bold.red('gulp')+chalk.yellow(' to start the app'));
-        });
-    }
+    }//end promtUser
+    //
+    //
+    // copyFiles: function(){
+    //
+    //     var approot = base+this.includeAppName+'/';
+    //
+    //     this.template('.gitignore',    approot+'.gitignore');
+    //     this.template('editorconfig',  approot+'editorconfig');
+    //     this.template('README.md',     approot+'README.md');
+    //     this.template('bower.json',     approot+'bower.json');
+    //
+    //     // passing app name to package.json
+    //     var appname = { clientname: this.includeAppName};
+    //     var localname = { localhost: this.includeLocalName};
+    //
+    //     this.template('package.json', approot+'package.json', appname);
+    //     this.template('gulpfile.js', approot+'gulpfile.js', localname);
+    //     this.template('_yo-rc.json', approot+'.yo-rc.json');
+    // },//end copy files
+    //
+    // move_ASSETS_Files: function(){
+    //
+    //     var approot = base+this.includeAppName+'/';
+    //     var clientname2 = { clientname: this.includeAppName};
+    //
+    //     this.template('source/index.php',approot+'source/index.php', clientname2);
+    //     this.template('source/js/main.js',approot+'source/js/main.js');
+    //     this.template('source/scss/main.scss',approot+'source/scss/main.scss');
+    //
+    //     // smacks
+    //     this.template('source/scss/vars/_base.scss',approot+'source/scss/vars/_base.scss');
+    //     this.template('source/scss/vars/_fonts.scss',approot+'source/scss/vars/_fonts.scss');
+    //     this.template('source/scss/vars/_state.scss',approot+'source/scss/vars/_state.scss');
+    //     this.template('source/scss/vars/_theme.scss',approot+'source/scss/vars/_theme.scss');
+    //     this.template('source/scss/vars/_layout.scss',approot+'source/scss/vars/_layout.scss');
+    //     this.template('source/scss/vars/_navbar.scss',approot+'source/scss/vars/_navbar.scss');
+    //     this.template('source/scss/vars/_vars.scss',approot+'source/scss/vars/_vars.scss');
+    //
+    // }, //end move_ASSETS_Files
+    // end: function () {
+    //
+    //     this.log(chalk.yellow(
+    //         '\n Go and get yourself a coffee, It will take a while...'
+    //     ));
+    //
+    //     //  new folder thats created with the appname
+    //
+    //     var approot = "\\"+base+this.includeAppName+"\\";
+    //
+    //     // add that to the current directory
+    //     var npmdir = process.cwd()+approot;
+    //
+    //     process.chdir(npmdir);
+    //
+    //     this.installDependencies({
+    //         callback: function(){
+    //             this.log(chalk.yellow(
+    //                 '\n Enjoying your coffee ? I\'m almost done...'
+    //             ));
+    //             setTimeout(function(){
+    //                 this.spawnCommand('gulp',['build']);
+    //                 this.spawnCommand('gulp',['vendor']);
+    //             }, 3000);//sleep for 3 secs
+    //         }.bind(this) // bind the callback to the parent scope
+    //     });
+    // },
+    // whatToDoNext: function() {
+    //     this.on('end', function(){
+    //         this.log(chalk.yellow("\n run")+chalk.bold.red('gulp')+chalk.yellow(' to start the app'));
+    //     });
+    // }
 });
 module.exports = myflow;
