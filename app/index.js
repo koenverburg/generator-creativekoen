e38a86065fabe260445f4aea47d7606db2feed97
CreativeKoen <CreativeKoen@gmail.com>
2015-06-11 21:17:15 +0200

    fixed preprocessor questions and added question for html/php

--- app/index.js
+++ app/index.js
@@ -5,15 +5,18 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var inquirer = require('inquirer');
-// var _ = require('underscore').string;
 
 var base = 'assets';
 
 var myflow = module.exports = function myflow(args, options){
     yeoman.generators.Base.apply(this, arguments);
     this.welcome;
-
-
+    this.on('end', function(){
+        this.installDependencies({
+            skipInstall: options['skip-install'],
+            skipMessage: options['skip-install']
+        })
+    });
 };
 
 util.inherits(myflow, yeoman.generators.Base);
@@ -52,54 +55,30 @@ myflow.prototype.askForCss = function askForCss() {
         checked: false
       }]
     },
-    // ask for sass deps
+    //ask for sass deps
     {
         when: function (answers) {
         return answers && answers.features &&
           answers.features.indexOf('includeSass') !== -1;
         },
         type: 'confirm',
-        name: 'bourbon',
-        value: 'includeBourbon',
-        message: 'Would you like to use bourbon?',
-        default: false
-    }, {
-        when: function (answers) {
-        return answers && answers.features &&
-          answers.features.indexOf('includeSass') !== -1;
-        },
-        type: 'confirm',
-        name: 'breakpoint',
-        value: 'includeBreakpoint',
-        message: 'Would you like to use breakpoint?',
+        name: 'SassStack',
+        value: 'includeSassStack',
+        message: 'Would you like to use Bourbon,breakpoint and Jeet?',
         default: false
     },
-    // ask for stylus deps
+    //ask for stylus deps
     {
         when: function (answers) {
         return answers && answers.features &&
           answers.features.indexOf('includeStylus') !== -1;
         },
         type: 'confirm',
-        name: 'nib',
-        value: 'includeNib',
-        message: 'Would you like to use nib?',
-        default: false
-    }, {
-        when: function (answers) {
-        return answers && answers.features &&
-          answers.features.indexOf('includeStylus') !== -1;
-        },
-        type: 'confirm',
-        name: 'rupture',
-        value: 'includeRupture',
-        message: 'Would you like to use rupture?',
-        default: false
+        name: 'StylusStack',
+        value: 'includeStylusStack',
+        message: 'Would you like to use Nib, Rupture and Jeet?',
+        default: true
     }
-
-
-
-
     ];
 
     this.prompt(prompts, function (answers) {
@@ -110,27 +89,62 @@ myflow.prototype.askForCss = function askForCss() {
         }
 
         this.includeSass = hasFeature('includeSass');
-
-        this.includeBourbon = answers.includeBourbon;
-        this.includeBourbon = !answers.includeNib;
-
-        this.includeBreakpoint = answers.includeBreakpoint;
-        this.includeBreakpoint = !answers.includeRupture;
-
-        console.log(this.includeSass);
-        console.log(this.includeBourbon);
-        console.log(this.includeBreakpoint);
-
         this.includeStylus = hasFeature('includeStylus');
 
-        this.includeNib = answers.includeNib;
-        this.includeNib = !answers.includeBourbon;
-        this.includeRupture = answers.includeRupture;
-        this.includeRupture = !answers.includeBreakpoint;
+        this.includeStylusStack = hasFeature('includeStylusStack');
+        this.includeSassStack = hasFeature('includeSassStack');
 
-        console.log(this.includeStylus);
-        console.log(this.includeNib);
-        console.log(this.includeRupture);
+        // if (this.includeSass && this.includeSassStack) {
+        //    console.log('Sass with a glass of bourbon breakpoint and jeet');
+        // }
+
+        // if (this.includeStylus && this.includeStylusStack) {
+        //    console.log('stylus with nib rupter and jeet selected');
+        // }
+
+        done();
+    }.bind(this));
+};
+
+myflow.prototype.askForLost = function askForLost() {
+    var done = this.async();
+
+    var prompts = [{
+        type: 'checkbox',
+        name: 'features',
+        message: 'What kind to html templating engine do you want use?',
+        choices: [{
+                name: 'Html',
+                value: 'includeHtml',
+                checked: false
+            },{
+                name: 'Html (in jade)',
+                value: 'includeHtmlPhp',
+                checked: false
+            },{
+                name: 'Php',
+                value: 'includePhp',
+                checked: false
+            },{
+                name: 'Php (in jade)',
+                value: 'includePhpJade',
+                checked: false
+            }
+            ]
+        }];
+
+    this.prompt(prompts, function(answers){
+        var features = answers.features;
+        function hasFeature(feat){
+            return features && features.indexOf(feat) !== -1;
+        }
+
+        this.includeHtml = answers.includeHtml;
+        this.includeHtmlJade = answers.includeHtmlJade;
+
+        this.includePhp = answers.includePhp;
+        this.includePhpJade = answers.includePhpJade;
+
         done();
     }.bind(this));
 };
